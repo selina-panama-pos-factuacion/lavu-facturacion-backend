ce57711f6b9b24440ca30a990346f8b49779edaf
Gabriel Bermúdez <gbermudezmora@gmail.com>
2023-10-23 23:04:25 -0600

    route added

--- routes/facturar.routes.js
+++ routes/facturar.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { cierreDeDiaHandler, facturarHandler, fechaCierreDeDiaHandler } from '../handlers/facturar.handlers.js'
+import { cierreDeDiaHandler, facturarHandler, fechaCierreDeDiaHandler, cierreDeDiaPostHandler } from '../handlers/facturar.handlers.js'
 import { validateBearerToken } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
 router.post('/facturar', validateBearerToken, facturarHandler)
-router.get('/facturarCierreDeDia', cierreDeDiaHandler)
 router.get('/fechaCierreDeDia', validateBearerToken, fechaCierreDeDiaHandler)
+router.post('/facturarCierreDeDia', validateBearerToken, cierreDeDiaPostHandler)
+router.get('/facturarCierreDeDia', cierreDeDiaHandler)
 
 export default router
